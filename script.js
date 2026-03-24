cf8603f71a558f28fa537552dfee001a28a7bfcf
ishan5971 <ishanmurali1000@gmail.com>
2026-03-24 13:05:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Productivity Cube - Design Thinking Project
 // ================================================
 
-// --- ⚠️ BLUETOOTH CONFIGURATION ⚠️ ---
+// --- BLUETOOTH CONFIGURATION ---
 const BLE_DEVICE_NAME = 'ProductivityCube'; 
 const BLE_SERVICE_UUID = '4fafc201-1fb5-459e-8fcc-c5c9c331914b'; 
 const BLE_CHARACTERISTIC_UUID = 'beb5483e-36e1-4688-b7f5-ea07361b26a8'; 
